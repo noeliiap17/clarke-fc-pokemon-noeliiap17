fd7508229d2f37e94425d83f58c2280cda8c2808
noeliiap17 <n.parra2003@gmail.com>
2018-03-12 11:42:01 +0100

    PaintPokemons

--- src/App.js
+++ src/App.js
@@ -5,46 +5,49 @@ import PokemonList from './components/PokemonList';
 class App extends Component {
   constructor(props){
     super(props);
-		// this.handleChange = this.handleChange.bind(this)
+		this.handleChange = this.handleChange.bind(this)
     this.state = {
       pokemons: [],
 			valorInput:''
     };
   }
-  componentDidMount() {
-    for (let i = 1; i <= 25; i++) {
-		fetch (`https://pokeapi.co/api/v2/pokemon/${i}/`)
-		.then(request => request.json())
-		.then(json => {
-			this.setState({
-				pokemons: json,
-			});
-			// console.log(pokemons);
-		});
-	}
-}
-  paintPokemons() {
-		let listPokemons = this.state.pokemons;
-
-    listPokemons = listPokemons.filter(element => element.name.toLowerCase().includes(this.state.valorInput.toLowerCase())
-    );
-
-    listPokemons = listPokemons.sort(function (a, b){
-      return a.id - b.id;
-    });
-
-			return (<section className="container">
-         <div></div>
-        <PokemonList poke={listPokemons} observer={this.observer} />
-      </section>)
-		};
-
+  componentDidMount(){
+  		for (let pokemonID = 1; pokemonID <=25; pokemonID++) {
+  		fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}/`)
+  		.then(response => response.json())
+  		.then(results => {
+  			let listPokemons = this.state.pokemons;
+  			listPokemons.push(results)
+  			this.setState({
+  				pokemons: listPokemons
+  			});
+  		})
+  	}
+  	}
 handleChange(event){
   const searchCharacters = event.target.value
   this.setState({
     valorInput: searchCharacters.toLowerCase()
   });
 }
+  paintPokemons() {
+		let listPokemons = this.state.pokemons;
+
+    listPokemons = listPokemons.filter(element => element.name.toLowerCase().includes(this.state.valorInput.toLowerCase())
+    );
+    console.log(this.state.pokemons)
+
+    listPokemons = listPokemons.sort(function (a, b){
+      return a.id - b.id;
+    });
+
+			return (
+				listPokemons.map((pokemon, i) =>
+								<PokemonList key={i} id={pokemon.id} name={pokemon.name} type={pokemon.types} image={pokemon.sprites.front_default} />)
+		);
+		};
+
+
   render() {
     return (
       <div className="App">
@@ -57,8 +60,8 @@ handleChange(event){
               <input className="search" placeholder=" Encuentra tu Pokémon favorito" onChange={this.handleClick}></input>
             </header>
             <main>
-            <div className="Container-page">
-    					<ul className="Container-pokemons">{this.paintPokemons()}</ul>
+            <div className="Container__page">
+    					<ul className="Container__pokemons">{this.paintPokemons()}</ul>
     				</div>
             </main>
           </div>
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import CardPokemon from './CardPokemon';
+import React, { Component } from 'react';
 
-export default class PokemonList extends React.Component {
-  render(){
-
-    return(
-      <ul className="">
-        {
-          this.props.poke.map(({ id, sprites,name, types}) =>
-            <CardPokemon key={id} number={id} sprite={sprites.front_default} name={name} observer={this.props.observer}
-            types= {types.sort((typeNumber) => typeNumber.slot).map((typeNumber) => typeNumber.type.name)} />
-          )
-        }
-      </ul>
-    )
-  }
+class Pokemons extends Component{
+	constructor(props){
+		super(props);
+		this.addType = this.addType.bind(this);
+		this.state = {
+			pokemonType: this.props.type
+		};
+	}
+	addType(){
+		let type = '';
+		for (let i = 0; i< this.state.pokemonType.length; i++){
+			type = type + ' ' + this.state.pokemonType[i].type.name;
+		}
+		return(
+			<p className="ch-box">
+				{type}
+			</p>
+		)
+	}
+	render(){
+		const styleImage = {
+			backgroundImage: `url(${this.props.image})`
+		}
+		return(
+			<li className="pokemon__card">
+					<div className="image" style = {styleImage}>
+						<img height="0" width="0" src ={this.props.image} alt="Pokemons"></img>
+            <div className="image__id">
+              <p className="id">ID/ {this.props.id}</p>
+            </div>
+					</div>
+          <div className="info">
+            <h2 className="name">{this.props.name}</h2>
+            <div className="characteristics">
+              <p>{this.addType()}</p>
+            </div>
+          </div>
+			</li>
+		);
+	}
 }
+
+export default Pokemons;
